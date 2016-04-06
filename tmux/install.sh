2e1e735df1d1009835b8d442ac390d06c63bd12f
jordanandree <jordanandree@gmail.com>
2016-04-06 11:02:46 -0400

    add power line symbols font

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,4 +4,6 @@ if [ ! -d ~/src/powerline-fonts ]; then
   mkdir -p ~/src/
   git clone https://github.com/powerline/fonts.git  ~/src/powerline-fonts
   (cd ~/src/powerline-fonts && ./install.sh)
+
+  curl -o ~/Library/Fonts/PowerlineSymbols.otf https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 fi;
